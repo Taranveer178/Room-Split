d108d3551d5b7346872fe8b86891827972a0ddde
Taranveer Singh <staranveer178@gmail.com>
2026-09-19 16:36:00 +0530

    origin main

--- roomsplit/src/App.jsx
+++ roomsplit/src/App.jsx
@@ -1,15 +1,21 @@
 import Header from './components/Header'
 import Welcome from './components/Welcome'
 import GroupCard from './components/GroupCard'
+import {useState} from 'react'
 
 function App() {
-  const groups=[
+  const [count, setCount] =useState(0)
+  const [groups, setGroups]=useState([
     {id:1, name:"Roomate", members:3, totalExpense:1000},
     {id:2, name:"College Friends", members:5, totalExpense:1500},
     {id:3, name:"Goa Trip", members:6, totalExpense:16500},
     {id:4, name:"Office Colleges", members:4, totalExpense:3000},
-  ]
-  
+  ])
+
+  function addGroup(){
+    const newGroup=  {id:5, name:"Party", members:3, totalExpense:1500}
+    setGroups([...groups, newGroup])
+  }
   return (
     <div>
       <Header />
@@ -22,6 +28,8 @@ function App() {
         totalExpense={e.totalExpense}
         />
       ))}
+      <h2>Count: {count}</h2>
+      <button onClick={addGroup}> Add Group</button>
     </div>
   )
 }
--- roomsplit/src/test.jsx
+++ roomsplit/src/test.jsx
@@ -1,28 +1,39 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Welcome from './components/Welcome'
 import GroupCard from './components/GroupCard'
 
 function App() {
-  const groups=[
-    {id:1, name:"Roomate", members:3, totalExpense:1000},
-    {id:2, name:"College Friends", members:5, totalExpense:2000},
-    {id:3, name:"Goa Trip", members:4, totalExpense:1500},
-    {id:4, name:"Office Friends", members:4, totalExpense:3000}
+
+  const [count, setCount] = useState(0)
+
+  const groups = [
+    { id: 1, name: "Roomate", members: 3, totalExpense: 1000 },
+    { id: 2, name: "College Friends", members: 5, totalExpense: 2000 },
+    { id: 3, name: "Goa Trip", members: 4, totalExpense: 1500 },
+    { id: 4, name: "Office Friends", members: 4, totalExpense: 3000 }
   ]
+
   return (
     <div>
       <Header />
-      <Welcome name="Taran" />
-      {groups.map((e)=>(
-        <GroupCard
-        key={e.id} 
-        name={e.name}
-        members={e.members}
-        totalExpense={e.totalExpense}
-        />
-      )
 
-      )}
+      <Welcome name="Taran" />
+
+      <h2>Count: {count}</h2>
+
+      <button onClick={() => setCount(count + 1)}>
+        Increase
+      </button>
+
+      {groups.map((e) => (
+        <GroupCard
+          key={e.id}
+          name={e.name}
+          members={e.members}
+          totalExpense={e.totalExpense}
+        />
+      ))}
     </div>
   )
 }
